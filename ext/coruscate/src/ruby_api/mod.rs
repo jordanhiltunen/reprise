5bd865dd4dbf1c86161f81888fc143d371e4031a
jordanwheatunen <hello@jordanhiltunen.com>
2024-06-17 20:37:44 -0400

    :wrench: ext/coruscate/src/ruby_api/mod.rs: Declare additional modules

--- ext/coruscate/src/ruby_api/mod.rs
+++ ext/coruscate/src/ruby_api/mod.rs
@@ -2,6 +2,10 @@ use magnus::prelude::*;
 use magnus::{Error, Ruby};
 
 mod schedule;
+mod occurrence;
+mod exclusion;
+mod traits;
+mod recurrence_rules;
 
 pub fn init(ruby: &Ruby) -> Result<(), Error> {
     schedule::init(ruby)?;
