f7da6886724ce87dc615a214a3b65ba9a04403df
jordanwheatunen <hello@jordanhiltunen.com>
2024-06-17 20:40:07 -0400

    :sparkles: ext/coruscate/src/ruby_api/traits.rs

---
+++ ext/coruscate/src/ruby_api/traits.rs
@@ -0,0 +1,21 @@
+use crate::ruby_api::occurrence::Occurrence;
+
+pub(crate) trait HasOverlapAwareness {
+    fn get_start_time(&self) -> i64;
+    fn get_end_time(&self) -> i64;
+
+    fn overlaps_with<T: HasOverlapAwareness>(&self, other: &T) -> bool {
+        dbg!("Other:");
+        dbg!(other.get_start_time(), other.get_end_time());
+
+        dbg!("Self:");
+        dbg!(self.get_start_time(), self.get_end_time());
+
+        return ((self.get_start_time() <= other.get_end_time())
+            && (other.get_start_time() < self.get_end_time()));
+    }
+}
+
+pub(crate) trait RecurrenceRule {
+    fn generate_occurrences(&self) -> Vec<Occurrence>;
+}
