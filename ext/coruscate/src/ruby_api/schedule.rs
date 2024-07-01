8f4b5b03d96c8e3427235508576ecad497956f0a
jordanwheatunen <hello@jordanhiltunen.com>
2024-06-30 20:27:38 -0400

    :recycle: Update exclusion handling in schedule.rs

--- ext/coruscate/src/ruby_api/schedule.rs
+++ ext/coruscate/src/ruby_api/schedule.rs
@@ -8,109 +8,85 @@ use chrono::{DateTime};
 use chrono_tz::Tz;
 use crate::ruby_api::occurrence::Occurrence;
 use crate::ruby_api::exclusion::Exclusion;
-use crate::ruby_api::traits::HasOverlapAwareness;
+use crate::ruby_api::traits::{HasOverlapAwareness, RecurringSeries};
+use crate::ruby_api::frequencies::weekly::Weekly;
+use crate::ruby_api::sorted_exclusions::SortedExclusions;
+use crate::ruby_api::time_of_day::TimeOfDay;
 
-// #[magnus::wrap(class = "Coruscate::Core::Schedule")]
+type UnixTimestamp = i64;
+type Second = i64;
+
 pub(crate) struct Schedule {
-    pub(crate) start_time: i64,
-    pub(crate) end_time: i64,
+    pub(crate) start_time: UnixTimestamp,
+    pub(crate) local_start_time: DateTime<Tz>,
+    pub(crate) end_time: UnixTimestamp,
+    pub(crate) local_end_time: DateTime<Tz>,
     pub(crate) time_zone: Tz,
     pub(crate) occurrences: Vec<Occurrence>,
-    pub(crate) exclusions: Vec<Exclusion>
+    pub(crate) sorted_exclusions: SortedExclusions
 }
 
 #[magnus::wrap(class = "Coruscate::Core::Schedule")]
 struct MutSchedule(RefCell<Schedule>);
 
 impl MutSchedule {
-    pub(crate) fn new(start_time: i64, end_time: i64, time_zone: String) -> MutSchedule {
+    pub(crate) fn new(start_time: UnixTimestamp, end_time: UnixTimestamp, time_zone: String) -> MutSchedule {
         let parsed_time_zone: Tz = time_zone.parse().expect("Cannot parse time zone");
 
+        let start_time_utc = DateTime::from_timestamp(start_time, 0).unwrap();
+        let local_start_time = start_time_utc.with_timezone(&parsed_time_zone);
+        let end_time_utc = DateTime::from_timestamp(end_time, 0).unwrap();
+        let local_end_time = end_time_utc.with_timezone(&parsed_time_zone);
+
         Self(RefCell::new(
-    Schedule {
-            start_time,
-            end_time,
-            time_zone: parsed_time_zone,
-            occurrences: Vec::new(),
-            exclusions: Vec::new(),
-        }))
+            Schedule {
+                start_time,
+                local_start_time,
+                end_time,
+                local_end_time,
+                time_zone: parsed_time_zone,
+                occurrences: Vec::new(),
+                sorted_exclusions: SortedExclusions::new()
+            }))
     }
 
-    // pub(crate) fn set_exclusions(self, exclusions: Vec<(i64,i64)>) -> bool {
-    pub(crate) fn set_exclusions(&self, exclusions: Vec<(i64, i64)>) -> bool {
-        let converted_exclusions = exclusions.iter().map(|e| Exclusion::new(e.0, e.1))
-            .collect::<Vec<Exclusion>>().try_into().unwrap();
-        self.0.borrow_mut().exclusions = converted_exclusions;
 
-        dbg!(&self.0.borrow().exclusions);
+    pub(crate) fn add_exclusions(&self, exclusions: Vec<(i64, i64)>) -> bool {
+        let mut converted_exclusions = exclusions.iter().map(|e| Exclusion::new(e.0, e.1))
+            .collect::<Vec<Exclusion>>();
 
-        return true
+        self.0.borrow_mut().sorted_exclusions.add_exclusions(&mut converted_exclusions);
+
+        return true;
     }
 
     pub(crate) fn add_exclusion(&self, start_time: i64, end_time: i64) -> bool {
-        self.0.borrow_mut().exclusions.push(
-            Exclusion { start_time, end_time }
-        );
+        self.0.borrow_mut().sorted_exclusions.add_exclusion(Exclusion {
+            start_time, end_time
+        });
 
-        dbg!("EXCLUSIONS");
-        dbg!(&self.0.borrow().exclusions);
-
-        return true
+        return true;
     }
 
-    pub(crate) fn occurrences(&self) -> Vec<i64> {
-        // TODO: only return occurrences that do not conflict with exclusions.
-        // TODO: generate occurrences on the basis of weekly / daily / yearly / monthly recurrence rules
+    pub(crate) fn repeat_weekly(&self, weekday_string: String, starts_at_time_of_day_ruby_hash: RHash, duration_in_seconds: i64) -> bool {
+        let starts_at_time_of_day = TimeOfDay::new_from_ruby_hash(starts_at_time_of_day_ruby_hash);
+        let mut new_occurrences: Vec<Occurrence> = Vec::new();
 
-        let start_time_dt: DateTime<Utc> = DateTime::from_timestamp(self.0.borrow().start_time, 0).unwrap();
-        dbg!(&start_time_dt);
-        let local_time = start_time_dt.with_timezone(&self.0.borrow().time_zone);
-
-        let start_time_hours = local_time.hour();
-        let start_time_minutes = local_time.minute();
-        let start_time_seconds = local_time.second();
-
-        dbg!("Start time details");
-        dbg!(start_time_hours, start_time_minutes);
-
-        let mut time_vec: Vec<MappedLocalTime<DateTime<Tz>>> = Vec::new();
-
-        for i in 0..12 {
-            let intermediate_time = start_time_dt.checked_add_days(Days::new(i)).unwrap();
-            let naive_time = NaiveTime::from_hms_opt(start_time_hours, start_time_minutes, start_time_seconds).unwrap();
-            let naive_intermediate_datetime = NaiveDate::from(intermediate_time.naive_utc()).and_time(naive_time);
-            let tz_aware_intermediate = self.0.borrow().time_zone.from_local_datetime(&naive_intermediate_datetime);
-
-            let occurrence = Occurrence {
-                start_time: tz_aware_intermediate.unwrap().timestamp(),
-                end_time: tz_aware_intermediate.unwrap().timestamp() + 3600,
-            };
-
-            let mut is_valid = true;
-            for exclusion in self.0.borrow().exclusions.iter() {
-                dbg!("CHECKING EXCLUSION");
-                dbg!(exclusion);
-
-                let e1 = DateTime::from_timestamp(exclusion.start_time, 0).unwrap();
-                let o1 = DateTime::from_timestamp(occurrence.start_time, 0).unwrap();
-
-                dbg!(e1, "EXCLUSION");
-                dbg!(o1, "OCCURRENCE");
-
-
-                if exclusion.overlaps_with(&occurrence) {
-                    dbg!("The occurrence is not valid");
-                    is_valid = false;
-                }
-            }
-
-            if is_valid {
-                dbg!("The occurrence is valid");
-                time_vec.push(tz_aware_intermediate);
-            }
+        {
+            let schedule_reference = self.0.borrow();
+            let weekly_series = Weekly::new(&schedule_reference, weekday_string, starts_at_time_of_day, duration_in_seconds);
+            new_occurrences = weekly_series.generate_occurrences();
         }
 
-        return time_vec.into_iter().map(|t| t.unwrap().timestamp()).collect();
+        self.0.borrow_mut().occurrences.extend(new_occurrences);
+
+        return true;
+    }
+
+    pub(crate) fn get_occurrences(&self) -> Vec<Occurrence> {
+        return self.0.borrow().occurrences.clone().into_iter()
+            .filter(|o| !self.0.borrow().sorted_exclusions.is_occurrence_excluded(o))
+            .collect();
     }
 }
 
@@ -120,9 +96,10 @@ pub fn init(ruby: &Ruby) -> Result<(), Error> {
     let class = core_module.define_class("Schedule", class::object())?;
 
     class.define_singleton_method("new", function!(MutSchedule::new, 3))?;
-    class.define_method("occurrences", method!(MutSchedule::occurrences, 0))?;
-    class.define_method("set_exclusions", method!(MutSchedule::set_exclusions, 1))?;
+    class.define_method("occurrences", method!(MutSchedule::get_occurrences, 0))?;
     class.define_method("add_exclusion", method!(MutSchedule::add_exclusion, 2))?;
+    class.define_method("add_exclusions", method!(MutSchedule::add_exclusions, 1))?;
+    class.define_method("repeat_weekly", method!(MutSchedule::repeat_weekly, 3))?;
 
     Ok(())
 }
