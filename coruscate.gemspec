410d26cc5a90f66ae4657d4e755e18e739398648
jordanwheatunen <hello@jordanhiltunen.com>
2024-06-30 21:12:45 -0400

    :chart_with_upwards_trend: benchmark ice_cube vs. coruscate

--- coruscate.gemspec
+++ coruscate.gemspec
@@ -38,12 +38,14 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
   spec.extensions = %w[ext/coruscate/extconf.rb]
 
-
   spec.add_development_dependency "bundler", "~> 2.5.9"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rake-compiler", "~> 1.2.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "ice_cube"
+  spec.add_development_dependency "benchmark-ips"
+  spec.add_development_dependency "benchmark-memory"
 
-    spec.add_dependency "activesupport", "~> 7.0.8"
+  spec.add_dependency "activesupport", "~> 7.0.8"
   spec.add_dependency "rb_sys", ">= 0.9.86"
 end
--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "benchmark"
+require "ice_cube"
+require "benchmark/ips"
+require "benchmark/memory"
 
 RSpec.describe Coruscate::Schedule do
   subject(:schedule) do
@@ -95,6 +99,51 @@ RSpec.describe Coruscate::Schedule do
              "Tue Jul 16 2024 01:02AM -1000",
              "Tue Jul 23 2024 01:02AM -1000"
            )
+    end
+  end
+
+  describe "Benchmarks" do
+    def generate_ice_cube_occurrences
+      schedule = IceCube::Schedule.new(now = Time.current) do |s|
+        s.add_recurrence_rule(IceCube::Rule.weekly(1, :tuesday).until(Date.today + 365))
+        s.add_recurrence_rule(IceCube::Rule.weekly(1, :wednesday).until(Date.today + 365))
+        s.add_exception_time(now + 1.day)
+      end
+
+      schedule.all_occurrences.size
+    end
+
+    def generate_coruscate_occurrences
+      schedule = Coruscate::Schedule.new(
+        start_time: Time.current,
+        end_time: Time.current + 365.days,
+        time_zone: time_zone
+      )
+
+      schedule.repeat_weekly("tuesday", { hour: 1, minute: 2, second: 3 }, 300)
+      schedule.repeat_weekly("wednesday", { hour: 1, minute: 2, second: 3 }, 300)
+      schedule.add_exclusion(
+        (Time.current.in_time_zone("Hawaii") - 30.minutes).to_i,
+        (Time.current.in_time_zone("Hawaii") + 5.minutes).to_i
+      )
+
+      schedule.occurrences.size
+    end
+
+    it "is faster than ice cube" do
+      Benchmark.ips do |x|
+        x.report("IceCube:") { generate_ice_cube_occurrences }
+        x.report("Coruscate:") { generate_coruscate_occurrences }
+      end
+    end
+
+    it "is more memory-efficient than ice cube" do
+      Benchmark.memory do |x|
+        x.report("IceCube:") { generate_ice_cube_occurrences }
+        x.report("Coruscate:") { generate_coruscate_occurrences }
+
+        x.compare!
+      end
     end
   end
 end
