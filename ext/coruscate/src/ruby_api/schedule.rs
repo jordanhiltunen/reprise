869e6b0c656c8d4d16a36eb57e8e62a838d95113
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-06 06:38:32 -0400

    :recycle: Opt for a RwLock over a mutex

--- ext/coruscate/src/ruby_api/schedule.rs
+++ ext/coruscate/src/ruby_api/schedule.rs
@@ -1,5 +1,5 @@
 use std::sync::{Arc};
-use parking_lot::{Mutex};
+use parking_lot::{RwLock};
 use magnus::prelude::*;
 use magnus::{Error, Module, RHash, scan_args};
 use magnus::class;
@@ -7,6 +7,7 @@ use magnus::function;
 use magnus::method;
 use chrono::{DateTime};
 use chrono_tz::Tz;
+use rayon::iter::ParallelIterator;
 use crate::ruby_api::occurrence::Occurrence;
 use crate::ruby_api::exclusion::Exclusion;
 use crate::ruby_api::traits::{HasOverlapAwareness, RecurringSeries};
@@ -42,7 +43,7 @@ pub(crate) struct Schedule {
 
 #[derive(Debug)]
 #[magnus::wrap(class = "Coruscate::Core::Schedule")]
-struct MutSchedule(Arc<Mutex<Schedule>>);
+struct MutSchedule(Arc<RwLock<Schedule>>);
 
 impl MutSchedule {
     pub(crate) fn new(starts_at: UnixTimestamp, ends_at: UnixTimestamp, time_zone: String) -> MutSchedule {
@@ -52,7 +53,7 @@ impl MutSchedule {
         let ends_at_utc = DateTime::from_timestamp(ends_at, 0).unwrap();
         let local_ends_at = ends_at_utc.with_timezone(&parsed_time_zone);
 
-        Self(Arc::new(Mutex::new(
+        Self(Arc::new(RwLock::new(
             Schedule {
                 starts_at,
                 local_starts_at,
@@ -69,7 +70,7 @@ impl MutSchedule {
         let mut converted_exclusions = exclusions.iter().map(|e| Exclusion::new(e.0, e.1))
             .collect::<Vec<Exclusion>>();
 
-        self.0.lock().sorted_exclusions.add_exclusions(&mut converted_exclusions);
+        self.0.write().sorted_exclusions.add_exclusions(&mut converted_exclusions);
     }
 
     pub(crate) fn add_exclusion(&self, kw: RHash) {
@@ -78,7 +79,7 @@ impl MutSchedule {
         ).unwrap();
         let (start_at_unix_timestamp, end_at_unix_timestamp): (i64, i64) = args.required;
 
-        self.0.lock().sorted_exclusions.add_exclusion(Exclusion {
+        self.0.write().sorted_exclusions.add_exclusion(Exclusion {
             start_time: start_at_unix_timestamp,
             end_time: end_at_unix_timestamp,
         });
@@ -91,13 +92,13 @@ impl MutSchedule {
         let (initial_time_of_day, duration_in_seconds): (RHash, i64) = args.required;
         let starts_at_time_of_day = TimeOfDay::new_from_ruby_hash(initial_time_of_day);
         let hourly_series = Hourly::new(starts_at_time_of_day, duration_in_seconds);
-        self.0.lock().frequencies.push(Frequencies::Hourly(hourly_series));
+        self.0.write().frequencies.push(Frequencies::Hourly(hourly_series));
     }
 
     pub(crate) fn repeat_weekly(&self, weekday_string: String, starts_at_time_of_day_ruby_hash: RHash, duration_in_seconds: i64) {
         let starts_at_time_of_day = TimeOfDay::new_from_ruby_hash(starts_at_time_of_day_ruby_hash);
         let weekly_series = Weekly::new(weekday_string, starts_at_time_of_day, duration_in_seconds);
-        self.0.lock().frequencies.push(Frequencies::Weekly(weekly_series));
+        self.0.write().frequencies.push(Frequencies::Weekly(weekly_series));
     }
 
     pub(crate) fn repeat_monthly_by_day(&self, day_number: u32, starts_at_time_of_day_ruby_hash: RHash, duration_in_seconds: i64) {
@@ -107,8 +108,12 @@ impl MutSchedule {
     }
 
     pub(crate) fn occurrences(&self) -> Vec<Occurrence> {
-        let self_reference = self.0.lock();
+        let self_reference = self.0.read();
 
+        // Even though we are structurally equipped to use par_iter() here to parallelize,
+        // and it is a simple substitution over iter(), schedule expansion is not computationally
+        // demanding enough for it to really matter. Relative to IceCube, parallel processing is
+        // ~500x faster, and parallel, only ~200x.
         return self_reference.frequencies.iter().
             map(|series| {
                 return match series {
