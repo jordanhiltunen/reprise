652a994d2a6aa63510ea875cf9f1063ddac89e75
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-07 10:11:50 -0400

    :rotating_light: coruscate.rb: alphabetize imports

--- lib/coruscate.rb
+++ lib/coruscate.rb
@@ -3,7 +3,7 @@
 require "active_support"
 require "active_support/core_ext/integer/time"
 require "active_support/core_ext/time"
-require_relative "coruscate/version"
 require_relative "coruscate/coruscate"
 require_relative "coruscate/schedule"
 require_relative "coruscate/time_of_day"
+require_relative "coruscate/version"
