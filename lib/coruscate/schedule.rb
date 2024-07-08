6504672626e7be6cddd37b9dee1dcb27222277a8
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-08 07:14:48 -0400

    :recycle: Add wrapper interfaces for #repeat_hourly and #repeat_daily

--- lib/coruscate/schedule.rb
+++ lib/coruscate/schedule.rb
@@ -19,8 +19,6 @@ module Coruscate
     #   @option time_of_day [Integer] :hour, >= 0 && <= 23
     #   @option time_of_day [Integer] :minute, >= 0 && <= 59
     #   @option time_of_day [Integer] :second, >= 0 && <= 59
-    #   @example
-    #     { hour: 1, minute: 30, second: 15 }
     #   @raise [UnsupportedTypeError] if +time_of_day+ is neither a +Hash+ nor a +Time+.
     #   @raise [InvalidHashError] if the hash representation of the time is invalid.
     #   @raise [RangeError] if either the hour, minute, or second is out-of-range.
@@ -36,7 +34,7 @@ module Coruscate
     # @param ends_at [Time]
     #   The end of the schedule; the latest possible moment for a valid occurrence.
     # @param time_zone [String]
-    #   Must be an unambiguous, valid time-zone according to `ActiveSupport::TimeZone::find_tzinfo`.
+    #   Must be an unambiguous, valid time-zone according to +ActiveSupport::TimeZone::find_tzinfo+.
     #   See https://github.com/tzinfo/tzinfo/issues/53
     # @raise [TZInfo::InvalidTimezoneIdentifier] if the time zone is ambiguous or invalid.
     def initialize(starts_at:, ends_at:, time_zone:)
@@ -51,10 +49,35 @@ module Coruscate
       internal_schedule.occurrences
     end
 
+    # @!macro time_of_day
+    # @param duration_in_seconds [Integer]
+    # @return [void]
+    def repeat_hourly(time_of_day: nil, duration_in_seconds:)
+      internal_schedule.repeat_hourly(
+        time_of_day: TimeOfDay.new(time_of_day || starts_at).to_h,
+        duration_in_seconds:
+      )
+    end
+
+    # @!macro time_of_day
+    # @param duration_in_seconds [Integer]
+    # @return [void]
+    def repeat_daily(time_of_day: nil, duration_in_seconds:)
+      internal_schedule.repeat_daily(
+        time_of_day: TimeOfDay.new(time_of_day || starts_at).to_h,
+        duration_in_seconds:
+      )
+    end
+
     # @!macro weekday
     # @!macro time_of_day
     # @param duration_in_seconds [Integer]
     # @return [void]
+    # @example with an +hms_opts+ +time_of_day+ hash
+    #   schedule.repeat_weekly(:monday, time_of_day: { hour: 6 }, duration_in_seconds: 30)
+    # @example with a local time for +time_of_day+
+    #   local_time = Time.current.in_time_zone(my_current_time_zone)
+    #   schedule.repeat_weekly(:monday, time_of_day: local_time, duration_in_seconds: 30)
     def repeat_weekly(weekday, time_of_day: nil, duration_in_seconds:)
       internal_schedule.repeat_weekly(
         weekday,
@@ -67,6 +90,8 @@ module Coruscate
     # @!macro time_of_day
     # @param duration_in_seconds [Integer]
     # @return [void]
+    # @example
+    #   schedule.repeat_monthly_by_day(15, time_of_day: { hour: 9 }, duration_in_seconds: 30)
     def repeat_monthly_by_day(day_number, time_of_day:, duration_in_seconds:)
       internal_schedule.repeat_monthly_by_day(
         day_number,
