815efdace964b70b4e2ff1859d65fe74445d5ffa
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-10 21:17:46 -0400

    :bug: Correct DST transition issues in src/ruby_api/recurring_series/daily.rs

--- ext/coruscate/src/ruby_api/recurring_series/daily.rs
+++ ext/coruscate/src/ruby_api/recurring_series/daily.rs
@@ -1,25 +1,23 @@
+use crate::ruby_api::series_options::SeriesOptions;
 use crate::ruby_api::time_of_day::TimeOfDay;
 use crate::ruby_api::traits::Recurrable;
-use chrono::{DateTime, Days};
+use chrono::{DateTime, Days, TimeDelta};
 use chrono_tz::Tz;
-use crate::ruby_api::series_options::SeriesOptions;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Daily {
-    pub(crate) series_options: SeriesOptions
+    pub(crate) series_options: SeriesOptions,
 }
 
 impl Daily {
     pub(crate) fn new(series_options: SeriesOptions) -> Daily {
-        return Daily {
-            series_options
-        };
+        return Daily { series_options };
     }
 }
 
 impl Recurrable for Daily {
     fn get_series_options(&self) -> &SeriesOptions {
-        return &self.series_options
+        return &self.series_options;
     }
 
     fn get_time_of_day(&self) -> &TimeOfDay {
@@ -37,13 +35,28 @@ impl Recurrable for Daily {
         // This will have confusing behaviour when a daily series is requested for a time that
         // intermittently falls within a time zone transition (e.g. DST / ST). Whenever possible,
         // we respect the _later_ interpretation of the time, biasing towards the new offset.
-        return datetime_cursor
-            .with_time(self.naive_starts_at_time()).latest()
+        return match datetime_cursor
+            .with_time(self.naive_starts_at_time())
+            .latest() {
+            Some(datetime_cursor) => { Some(datetime_cursor) },
+            None => {
+                // If there is no local time (e.g. because we're in a time gap,
+                // during a DST transition, bypassing that exact hour) we return
+                // the cursor alone.
+                return Some(datetime_cursor.clone())
+            }
+        }
     }
 
     fn advance_datetime_cursor(&self, datetime_cursor: &DateTime<Tz>) -> DateTime<Tz> {
-        return datetime_cursor
-            .checked_add_days(Days::new(1))
-            .expect("Datetime must advance by one day");
+        // If a time zone jumps from 2:00 AM to 3:00 PM for DST,
+        // then the local 2:30 AM time simply does not exist; in that scenario,
+        // we increment the UTC time and take the local time from that.
+        return datetime_cursor.checked_add_days(Days::new(1)).unwrap_or_else(||
+            datetime_cursor
+                .to_utc()
+                .checked_add_signed(TimeDelta::hours(24))
+                .unwrap().with_timezone(&datetime_cursor.timezone())
+        );
     }
 }
