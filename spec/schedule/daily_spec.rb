04997914e55b3b29b179014b3d37042621a1bc8c
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-13 21:48:17 -0400

    :white_check_mark: spec/schedule/daily_spec.rb: Improve DST/ST test coverage

--- spec/schedule/daily_spec.rb
+++ spec/schedule/daily_spec.rb
@@ -7,38 +7,68 @@ RSpec.describe "#repeat_daily", aggregate_failures: true do
 
   subject(:schedule) { Coruscate::Schedule.new(starts_at:, ends_at:, time_zone:) }
 
-  let(:starts_at) { Time.current.in_time_zone(time_zone) }
+  let(:starts_at) { TimeZoneHelpers::ONE_MINUTE_BEFORE_LA_TRANSITION_TO_DST_2024.in_time_zone(time_zone) }
   let(:ends_at) { starts_at + 5.days }
   let(:time_zone) { TimeZoneHelpers::LOS_ANGELES_TIME_ZONE }
 
-  def localized_occurrence_start_time(occurrence)
-    occurrence.start_time.in_time_zone(time_zone).strftime("%a %b %e %Y %I:%M%p %z")
+  it_behaves_like "a series that supports optional occurrence labels" do
+    let(:series_options_hash) { series_options(time_of_day: { hour: 1, minute: 2, second: 3 }) }
+    let(:occurrences) do
+      schedule.repeat_daily(**series_options_hash)
+      schedule.occurrences
+    end
   end
 
-  before { travel_to Time.new(2024, 6, 30, 0, 0, 0, "-07:00") }
+  it_behaves_like "a series that supports the duration_in_seconds argument" do
+    let(:series_options_hash) { series_options(time_of_day: { hour: 1, minute: 2, second: 3 }) }
+    let(:occurrences) do
+      schedule.repeat_daily(**series_options_hash)
+      schedule.occurrences
+    end
+  end
 
   it "generates an array of daily occurrences" do
     schedule.repeat_daily(**series_options(time_of_day: { hour: 1, minute: 2, second: 3 }))
 
     expect(schedule.occurrences.map { |o| localized_occurrence_start_time(o) })
       .to contain_exactly(
-        "Sun Jun 30 2024 01:02AM -0700",
-        "Mon Jul  1 2024 01:02AM -0700",
-        "Tue Jul  2 2024 01:02AM -0700",
-        "Wed Jul  3 2024 01:02AM -0700",
-        "Thu Jul  4 2024 01:02AM -0700"
+        "Mon Mar 11 2024 01:02AM -0700",
+        "Tue Mar 12 2024 01:02AM -0700",
+        "Wed Mar 13 2024 01:02AM -0700",
+        "Thu Mar 14 2024 01:02AM -0700",
+        "Fri Mar 15 2024 01:02AM -0700"
       )
   end
 
-  context "when the schedule straddles a transition from Standard Time to Daylight Savings Time (DST)" do
-    let(:starts_at) { TimeZoneHelpers.hours_before_los_angeles_transition_to_dst - 1.day }
+  context "when the schedule starts on a transition from Standard Time (ST) to Daylight Savings Time (DST)" do
+    let(:starts_at) { (TimeZoneHelpers::ONE_MINUTE_BEFORE_LA_TRANSITION_TO_DST_2024 - 1.day).in_time_zone(time_zone) }
+
+    it "generates an array of occurrences starting from the DST change" do
+      schedule.repeat_daily(time_of_day: nil, duration_in_seconds: 30.minutes)
+
+      expect(schedule.occurrences.map { |o| localized_occurrence_start_time(o) })
+        .to contain_exactly(
+        # N.B. The first occurrence begins
+        # after the local time gap, one
+        # hour forward.
+        "Sun Mar 10 2024 01:59AM -0800",
+        "Mon Mar 11 2024 01:59AM -0700",
+        "Tue Mar 12 2024 01:59AM -0700",
+        "Wed Mar 13 2024 01:59AM -0700"
+      )
+    end
+  end
+
+  context "when the schedule crosses a transition from Standard Time to Daylight Savings Time (DST)" do
+    let(:starts_at) do
+      (TimeZoneHelpers::ONE_MINUTE_BEFORE_LA_TRANSITION_TO_DST_2024 - 1.day).in_time_zone(time_zone)
+    end
 
     it "generates an array of daily occurrences across the DST change" do
       schedule.repeat_daily(**series_options)
 
       expect(schedule.occurrences.map { |o| localized_occurrence_start_time(o) })
         .to contain_exactly(
-          "Fri Mar  8 2024 10:15PM -0800",
           "Sat Mar  9 2024 10:15PM -0800",
           "Sun Mar 10 2024 10:15PM -0700",
           "Mon Mar 11 2024 10:15PM -0700",
@@ -67,8 +97,8 @@ RSpec.describe "#repeat_daily", aggregate_failures: true do
     end
   end
 
-  context "when the schedule straddles a transition from DST to Standard Time" do
-    let(:starts_at) { TimeZoneHelpers.hours_before_los_angeles_transition_to_st - 1.day }
+  context "when the schedule crosses a transition from Daylight Savings Time (DST) to Standard Time (ST)" do
+    let(:starts_at) { TimeZoneHelpers::ONE_MINUTE_BEFORE_LA_TRANSITION_TO_ST_2024 - 1.day }
 
     it "generates an array of daily occurrences across the ST change" do
       schedule.repeat_daily(**series_options(time_of_day: { hour: 1, minute: 15 }))
