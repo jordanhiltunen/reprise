b756110c7db9a3f2750e237a96244e259620d617
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-13 20:30:17 -0400

    :white_check_mark: spec/schedule/daily_spec.rb: simplify imports

--- spec/schedule/daily_spec.rb
+++ spec/schedule/daily_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "../support/series_helpers"
-require_relative "../support/time_zone_helpers"
+require "spec_helper"
 
 RSpec.describe "#repeat_daily", aggregate_failures: true do
   include SeriesHelpers
