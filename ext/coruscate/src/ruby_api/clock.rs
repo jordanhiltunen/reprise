117a8556fa9d3730c9e9863ca0fa344f66189261
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-13 20:49:52 -0400

    :recycle: ext/coruscate/src/ruby_api/clock.rs: local variable consistency

--- ext/coruscate/src/ruby_api/clock.rs
+++ ext/coruscate/src/ruby_api/clock.rs
@@ -5,16 +5,16 @@ use chrono_tz::Tz;
 pub(crate) fn advance_time_safely(
     datetime_cursor: &DateTime<Tz>,
     time_delta: TimeDelta,
-    time_of_day: NaiveTime,
+    naive_time: NaiveTime,
 ) -> DateTime<Tz> {
     let new_datetime_cursor = datetime_cursor.checked_add_signed(time_delta)
         .expect("Datetime must advance")
-        .with_time(time_of_day).latest();
+        .with_time(naive_time).latest();
 
     return match new_datetime_cursor {
         None => {
             let timezone = datetime_cursor.timezone();
-            let datetime_cursor_utc = datetime_cursor.with_time(time_of_day)
+            let datetime_cursor_utc = datetime_cursor.with_time(naive_time)
                 .latest()
                 .expect("Datetime must advance")
                 .to_utc();
