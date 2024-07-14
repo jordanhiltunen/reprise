e3cc1876ef6880c91d7e050c7af20e06f52cd4a5
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-13 20:33:00 -0400

    :white_check_mark: spec/support/time_zone_helpers.rb: DST/ST helper constants

--- spec/support/time_zone_helpers.rb
+++ spec/support/time_zone_helpers.rb
@@ -3,6 +3,13 @@
 module TimeZoneHelpers
   LOS_ANGELES_TIME_ZONE = "America/Los_Angeles"
 
+  # Daylight Saving Time (DST) will begin at 2 a.m. on Sunday, March 10, 2024; the clocks are set
+  # ahead by one hour.
+  ONE_MINUTE_BEFORE_LA_TRANSITION_TO_DST_2024 = Time.new(2024, 3, 10, 1, 59, 0, "-0800").freeze
+  # Standard Time (ST) will begin at 2:00 am on Sunday, November 3, 2024; the clocks are turned
+  # back by one hour.
+  ONE_MINUTE_BEFORE_LA_TRANSITION_TO_ST_2024 = Time.new(2024, 11, 3, 0, 59, 0, "-0700").freeze
+
   module_function
 
   def hours_before_los_angeles_transition_to_dst
