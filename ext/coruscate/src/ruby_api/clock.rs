d99bef08f760536be5b8b101bd3dc963a3e4e110
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-13 20:24:57 -0400

    :sparkles: set_datetime_cursor_safely()

--- ext/coruscate/src/ruby_api/clock.rs
+++ ext/coruscate/src/ruby_api/clock.rs
@@ -1,4 +1,4 @@
-use crate::ruby_api::time_of_day::TimeOfDay;
+use std::ops::Add;
 use chrono::{DateTime, NaiveTime, TimeDelta};
 use chrono_tz::Tz;
 
@@ -26,4 +26,17 @@ pub(crate) fn advance_time_safely(
         }
         Some(new_datetime_cursor) => new_datetime_cursor,
     };
+}
+
+pub(crate) fn set_datetime_cursor_safely(datetime_cursor: DateTime<Tz>, naive_time: NaiveTime) -> DateTime<Tz> {
+    return match datetime_cursor.with_time(naive_time).latest() {
+        None => {
+            // If there is no valid time, it means that we are in a gap in local time:
+            // the requested local time is missing / does not exist. This occurs when
+            // a local time clock is turned forwards during a transition. To compensate,
+            // we translate the requested time one hour ahead, out of the gap.
+            datetime_cursor.with_time(naive_time.add(TimeDelta::hours(1))).latest().unwrap()
+        }
+        Some(datetime_cursor) => { datetime_cursor }
+    }
 }
