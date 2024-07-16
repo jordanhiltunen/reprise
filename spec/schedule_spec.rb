41777b3de5e63ba2a61639e5d3e83cf4290eec67
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-16 06:20:45 -0400

    :white_check_mark: spec/schedule_spec.rb: remove Time.current use

--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -8,18 +8,16 @@ RSpec.describe Reprise::Schedule, aggregate_failures: true do
   subject(:schedule) { Reprise::Schedule.new(starts_at:, ends_at:, time_zone:) }
 
   let(:time_zone) { "Hawaii" }
-  let(:starts_at) { Time.current.in_time_zone(time_zone) }
-  let(:ends_at) { (Time.current + 4.weeks).in_time_zone(time_zone) }
-  let(:event_duration_in_seconds) { 300 }
-
-  before { travel_to Time.new(2024, 6, 30, 0, 0, 0, "-10:00") } # Hawaii
+  let(:starts_at) { Time.new(2024, 6, 30, 0, 0, 0, "-10:00") }
+  let(:ends_at) { (starts_at + 4.weeks) }
+  let(:event_duration_in_seconds) { 5.hours }
 
   describe "#initialize" do
     context "when the time zone is invalid" do
       let(:time_zone) { "nonsense" }
 
-      it "raises an ArgumentError" do
-        expect { schedule }.to raise_error(ArgumentError, "Invalid Timezone: nonsense")
+      it "raises a TZInfo::InvalidTimezoneIdentifier" do
+        expect { schedule }.to raise_error(TZInfo::InvalidTimezoneIdentifier, "Invalid identifier: nonsense")
       end
     end
 
@@ -69,23 +67,23 @@ RSpec.describe Reprise::Schedule, aggregate_failures: true do
   it "supports the accumulation of occurrences from multiple recurring series with their own independent bookends" do
     schedule.repeat_weekly(:tuesday,
                            time_of_day: { hour: 9, minute: 0 }, duration_in_seconds: 300,
-                           starts_at: Time.current + 2.weeks, ends_at:)
+                           starts_at: starts_at + 2.weeks, ends_at:)
     schedule.repeat_weekly(:wednesday,
                            time_of_day: { hour: 4, minute: 7, second: 4 }, duration_in_seconds: 300,
-                           ends_at: Time.current + 6.weeks)
+                           ends_at: starts_at + 6.weeks)
 
     expect(schedule.occurrences.size).to eq(8)
     expect(
       schedule.occurrences.map { |o| o.start_time.in_time_zone(time_zone).strftime("%a %b %e %Y %I:%M%p %z") }
     ).to contain_exactly(
-       "Wed Aug  7 2024 04:07AM -1000",
-       "Wed Jul  3 2024 04:07AM -1000",
-       "Wed Jul 10 2024 04:07AM -1000",
-       "Tue Jul 16 2024 09:00AM -1000", # Tuesday series comes in 2 weeks later.
-       "Wed Jul 17 2024 04:07AM -1000",
-       "Tue Jul 23 2024 09:00AM -1000",
-       "Wed Jul 24 2024 04:07AM -1000",
-       "Wed Jul 31 2024 04:07AM -1000"
+      "Wed Jul  3 2024 04:07AM -1000",
+      "Wed Jul 10 2024 04:07AM -1000",
+      "Tue Jul 16 2024 09:00AM -1000", # Tuesday series comes in 2 weeks later.
+      "Wed Jul 17 2024 04:07AM -1000",
+      "Tue Jul 23 2024 09:00AM -1000",
+      "Wed Jul 24 2024 04:07AM -1000",
+      "Wed Jul 31 2024 04:07AM -1000",
+      "Wed Aug  7 2024 04:07AM -1000"
      )
   end
 
@@ -110,7 +108,7 @@ RSpec.describe Reprise::Schedule, aggregate_failures: true do
       expect(first_occurrence.start_time).to be_a(Time)
       expect(first_occurrence.start_time.in_time_zone(time_zone).to_s).to eq("2024-06-30 00:01:02 -1000")
       expect(first_occurrence.end_time).to be_a(Time)
-      expect(first_occurrence.end_time.in_time_zone(time_zone).to_s).to eq("2024-06-30 00:06:02 -1000")
+      expect(first_occurrence.end_time.in_time_zone(time_zone).to_s).to eq("2024-06-30 05:01:02 -1000")
     end
 
     it "exposes an optional label on each occurrence" do
@@ -123,7 +121,7 @@ RSpec.describe Reprise::Schedule, aggregate_failures: true do
       first_occurrence = occurrences.first
 
       expect(first_occurrence.inspect).to eq(
-        "Occurrence { starts_at_unix_timestamp: 1719741662, ends_at_unix_timestamp: 1719741962, label: Some(\"My Weekly Occurrence\") }"
+        '<Reprise::Core::Occurrence start_time="2024-06-30 06:01:02 -0400" end_time="2024-06-30 11:01:02 -0400" label="My Weekly Occurrence">'
       )
     end
   end
