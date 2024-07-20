f82eb9211e98ba6fe6c0deac6d093ff8ee51812c
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-20 14:58:24 -0400

    :rotating_light: Lint build.rs

--- ext/reprise/build.rs
+++ ext/reprise/build.rs
@@ -1,7 +1,7 @@
 use std::{error::Error};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Propagate linking from rb-sys for usage in the Reprise
+    // Propagate linking from rb-sys for usage in the Reprise extension
     let _ = rb_sys_env::activate()?;
 
     Ok(())
