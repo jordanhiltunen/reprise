e824032e9be1ed7b4c9a31fc15d44a87ba05e3ce
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-20 11:23:38 -0400

    :alembic: Experiment with a root-level Cargo.toml

---
+++ ext/reprise/build.rs
@@ -0,0 +1,8 @@
+use std::{error::Error};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Propagate linking from rb-sys for usage in the Reprise
+    let _ = rb_sys_env::activate()?;
+
+    Ok(())
+}
