3c45319e2508f02da59f8556884befacd7093510
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-20 13:17:15 -0400

    :bug: Update version-specific require statement for reprise/reprise in reprise.rb

--- lib/reprise.rb
+++ lib/reprise.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
+begin
+  require "reprise/#{RUBY_VERSION.to_f}/reprise"
+rescue LoadError
+  require "reprise/reprise"
+end
+
 require "active_support"
 require "active_support/core_ext/integer/time"
 require "active_support/core_ext/time"
-require "reprise/reprise"
 require "reprise/schedule"
 require "reprise/time_of_day"
 require "reprise/time_zone_identifier"
