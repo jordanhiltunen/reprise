670b3acd1b8e55ce587ddf7684d76d41eea49de8
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-21 09:07:15 -0400

    :rotating_light: ext/reprise/src/ruby_api/recurring_series/mod.rs: rustfmt

--- ext/reprise/src/ruby_api/recurring_series/mod.rs
+++ ext/reprise/src/ruby_api/recurring_series/mod.rs
@@ -1,8 +1,7 @@
+pub mod annually_by_day;
+pub mod daily;
 pub mod hourly;
-pub mod weekly;
+pub mod minutely;
 pub mod monthly_by_day;
 pub mod monthly_by_nth_weekday;
-pub mod daily;
-pub mod minutely;
-
-pub mod annually_by_day;
+pub mod weekly;
