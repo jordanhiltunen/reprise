5b3c1b77b546f3d455d2329eaab6cb491652d96c
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-21 09:12:54 -0400

    :rotating_light: ext/reprise/src/ruby_api/time_of_day.rs: rustfmt

--- ext/reprise/src/ruby_api/time_of_day.rs
+++ ext/reprise/src/ruby_api/time_of_day.rs
@@ -1,13 +1,13 @@
 use chrono::{DateTime, Timelike};
 use chrono_tz::Tz;
-use magnus::RHash;
 use magnus::symbol::IntoSymbol;
+use magnus::RHash;
 
 #[derive(Debug, Clone)]
 pub(crate) struct TimeOfDay {
     pub(crate) hour: u32,
     pub(crate) minute: u32,
-    pub(crate) second: u32
+    pub(crate) second: u32,
 }
 
 impl TimeOfDay {
@@ -15,17 +15,23 @@ impl TimeOfDay {
         let ruby = magnus::Ruby::get().unwrap();
 
         return TimeOfDay {
-            hour: time_of_day.fetch::<_, u32>("hour".into_symbol_with(&ruby)).unwrap_or(0),
-            minute: time_of_day.fetch::<_, u32>("minute".into_symbol_with(&ruby)).unwrap_or(0),
-            second: time_of_day.fetch::<_, u32>("second".into_symbol_with(&ruby)).unwrap_or(0)
-        }
+            hour: time_of_day
+                .fetch::<_, u32>("hour".into_symbol_with(&ruby))
+                .unwrap_or(0),
+            minute: time_of_day
+                .fetch::<_, u32>("minute".into_symbol_with(&ruby))
+                .unwrap_or(0),
+            second: time_of_day
+                .fetch::<_, u32>("second".into_symbol_with(&ruby))
+                .unwrap_or(0),
+        };
     }
 
     pub(crate) fn new_from_local_time(local_time: DateTime<Tz>) -> TimeOfDay {
         return TimeOfDay {
             hour: local_time.hour(),
             minute: local_time.minute(),
-            second: local_time.second()
-        }
+            second: local_time.second(),
+        };
     }
 }
