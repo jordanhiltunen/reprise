180a207278885d8fb8895644bf2bcb9ed32338d6
jordanwheatunen <hello@jordanhiltunen.com>
2024-07-24 06:53:52 -0400

    :memo: Update comment

--- ext/reprise/src/ruby_api/recurrence_modifiers/by_month.rs
+++ ext/reprise/src/ruby_api/recurrence_modifiers/by_month.rs
@@ -1,6 +1,6 @@
 use crate::ruby_api::recurrence_modifiers::modifiable::Modifiable;
 
-/// The ByMinute modifier corresponds to iCalendar's:
+/// The ByMonth modifier corresponds to iCalendar's:
 /// bymodaylist = ( monthdaynum *("," monthdaynum) )
 /// monthnum = 1*2DIGIT; 1 to 12
 /// See: https://icalendar.org/iCalendar-RFC-5545/3-3-10-recurrence-rule.html
